603d37629bd8fe7568988701c966411b57b66f5d
Alexsander <alexsandergutierreez@gmail.com>
2022-05-18 21:59:42 -0300

    Aula

---
+++ POLIGON_API/Poligono.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POLIGON_API
+{
+    public abstract class Poligono
+    {
+        public List<double> Lados { get; set; }
+        public Poligono(List<double> Lados)
+        {
+            this.Lados = Lados;
+        }
+        public abstract double Area;
+        public double Perimetro()
+        {
+            return Lados.Sum();
+        }
+    }
+}
---
+++ POLIGON_API/Triangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POLIGON_API
+{
+    public class Triangulo : Poligono
+    {
+        public Triangulo(List<double> Lados) : base(Lados)
+        {
+
+        }
+
+        public override double Area()
+        {
+            if(Lados[0] == Lados[1] && Lados[1] == Lados[2]) 
+            {
+                return Math.Pow(Lados[0], 2) * Math.Sqrt(3) / 4;
+            }
+            else if (Lados[0] != Lados[1] && Lados[1] != Lados[2] && Lados[0] != Lados[2])
+            {
+                double p = Perimetro() / 2;
+                return Math.Sqrt(p * (p - Lados[0]) * (p - Lados[1]) * (p - Lados[2]));
+            }
+            else 
+            {
+                double h = Math.Sqrt(Math.Pow(Lados[0], 2) - Math.Pow(Lados[1] / 2, 2));
+                return (Lados[2] * h) / 2;
+            }
+        }
+    }
+}
